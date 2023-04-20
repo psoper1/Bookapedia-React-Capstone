936cf96e92b744b7220f0934a104ece7da4f68bb
reeman9000 <preston.soper@student.awesomeinc.org>
2023-04-20 17:51:56 +0000

    added hamburger icon for mobile breakpoints

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -19,9 +19,8 @@ function Nav() {
             currentUser: null
         })
     }
-    
+
     const loadUser = async () => {
-        console.log(state.currentUser.user_id)
         try {
             let options = {
                 url: `users/${state.currentUser.user_id}/`,
@@ -30,11 +29,11 @@ function Nav() {
             let response = await request(options)
             console.log(response.data)
             setData(response.data)
-        } 
+        }
         catch (error) {
             // console.log(error);
             if (state.currentUser === null) {
-                
+
             } else {
                 console.log(error)
             }
@@ -53,15 +52,18 @@ function Nav() {
             <nav className="navbar navbar-expand-lg navbar-light">
                 <div className="container-fluid">
                     <NavLink to="/" className="nav-link active">Bookapedia Home</NavLink>
-                    <div className="nav justify-content-end navbar-collapse" id="navbarNavAltMarkup">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
                                 <NavLink to="/book-of-the-week" className="nav-link">Check out the Book of the Week here!</NavLink>
                             </li>
                             {state.currentUser && data &&
-                            <li className="nav-item">
-                                <NavLink to="/my-bookshelf" className="nav-link">{data.first_name}'s Bookshelf</NavLink>
-                            </li>
+                                <li className="nav-item">
+                                    <NavLink to="/my-bookshelf" className="nav-link">{data.first_name}'s Bookshelf</NavLink>
+                                </li>
                             }
                             {/* {data.first_name} */}
                             <li className="nav-item">
